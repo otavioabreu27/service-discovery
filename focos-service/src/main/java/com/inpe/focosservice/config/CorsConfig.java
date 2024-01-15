93e5948d1e0a0dd439278297a25d0929d7ac34d9
Otávio Abreu dos Santos Silva <otavio.abreu96@gmail.com>
2024-01-15 09:20:14 -0300

    fix cors error

--- focos-service/src/main/java/com/inpe/focosservice/config/CorsConfig.java
+++ focos-service/src/main/java/com/inpe/focosservice/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://locahost:3000")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
                 .allowCredentials(true)
